6556eea6b7fc3afb86ca73acac41e74f02bb7926
kashj22 <kjavaid22@gmail.com>
2015-11-02 19:33:35 +0000

    added reviews model but still having problem with caves name

--- shisha_app/app/models/image.rb
+++ shisha_app/app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ActiveRecord::Base
-  belongs_to :cafe
+  belongs_to :venue
   belongs_to :user
 end
--- shisha_app/config/routes.rb
+++ shisha_app/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :reviews
   resources :venues
   resources :images
   resources :locations
   devise_for :users
 
   root 'static_pages#home'
+  
 end
