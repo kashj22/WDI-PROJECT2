877001cbbfcb3c079384426a4569b823bfe7b256
kashj22 <kjavaid22@gmail.com>
2015-11-03 16:45:29 +0000

    added logo field to venue table

--- shisha_app/db/schema.rb
+++ shisha_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103160745) do
+ActiveRecord::Schema.define(version: 20151103163753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20151103160745) do
     t.integer  "location_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "logo"
   end
 
   add_index "venues", ["location_id"], name: "index_venues_on_location_id", using: :btree
