ada0d8ebb48e838cff79cf08cca0045160bd086d
kashj22 <kjavaid22@gmail.com>
2015-11-03 18:15:13 +0000

    added acts_as_votable

--- shisha_app/app/models/venue.rb
+++ shisha_app/app/models/venue.rb
@@ -2,4 +2,7 @@ class Venue < ActiveRecord::Base
   belongs_to :location
   has_many :reviews
   has_many :images
+
+  mount_uploader :logo, VenueLogoUploader
+  acts_as_votable
 end
--- shisha_app/db/schema.rb
+++ shisha_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103163753) do
+ActiveRecord::Schema.define(version: 20151103181126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,21 @@ ActiveRecord::Schema.define(version: 20151103163753) do
   end
 
   add_index "venues", ["location_id"], name: "index_venues_on_location_id", using: :btree
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
   add_foreign_key "reviews", "venues"
   add_foreign_key "venues", "locations"
