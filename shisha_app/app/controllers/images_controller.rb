ef23bd61fd0d8936063e97ff24fae38aad4be5dd
kashj22 <kjavaid22@gmail.com>
2015-11-03 16:37:05 +0000

    added functionality, photos now show on venue show page

--- shisha_app/app/controllers/images_controller.rb
+++ shisha_app/app/controllers/images_controller.rb
@@ -69,6 +69,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:image_url, :caption, :pics, :cafe_id)
+      params.require(:image).permit(:image_url, :caption, :pics, :venue_id)
     end
 end
--- shisha_app/app/models/venue.rb
+++ shisha_app/app/models/venue.rb
@@ -1,4 +1,5 @@
 class Venue < ActiveRecord::Base
   belongs_to :location
   has_many :reviews
+  has_many :images
 end
