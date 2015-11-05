7150ab3e7b4d13dd022be472c3fccf21128bc27c
kashj22 <kjavaid22@gmail.com>
2015-11-05 10:18:52 +0000

    styled the buttons for sign in, register and go to cafes

--- shisha_app/app/controllers/application_controller.rb
+++ shisha_app/app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+    def after_sign_in_path_for(resource)
+      venues_path(resource)
+    end
+
     # my custom fields are :name, :heard_how
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) do |u|
